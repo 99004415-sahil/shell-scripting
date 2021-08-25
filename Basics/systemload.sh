1247d6349d53dd7c555a2473a91596efe9729e73
Sahil Bindal <84443205+99004415-sahil@users.noreply.github.com>
2021-08-25 19:04:20 +0530

    Update systemload.sh

--- Basics/systemload.sh
+++ Basics/systemload.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
-##################################################
-# #
-# Author: Ankam Ravi Kumar #
-# Website: server-computer.com #
-# Date: 23-02-2019 16:59:56 #
-# Purpose: Capture and Store System Load Average #
-# CPU Usage and Memory Usage #
-##################################################
+
 # Log File Path
 LOGFILE=/var/log/systemload.log
 
